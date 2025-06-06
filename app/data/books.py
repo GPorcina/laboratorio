fedd316fb63a54d92ab667bc989d94cb49fcab30
GPorcina <giordano.porcina1@gmail.com>
2025-06-06 14:36:14 +0200

    Aggiunta struttura progetto e primi file API

---
+++ app/data/books.py
@@ -0,0 +1,7 @@
+from app.models.book import Book
+
+books = {
+    0: Book(id=0, title="libro zero", author="autore zero", review=3),
+    1: Book(id=1, title="libro uno", author="autore uno", review=5),
+    2: Book(id=1, title="libro due", author="autore due", review=1),
+}
